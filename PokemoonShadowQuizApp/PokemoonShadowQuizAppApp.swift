98a21c61d3f654540e63deee0e9665941b60c340
Bartosz <par-gluten-0f@icloud.com>
2025-07-14 17:08:34 +0200

    Initial Commit

---
+++ PokemoonShadowQuizApp/PokemoonShadowQuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemoonShadowQuizAppApp.swift
+//  PokemoonShadowQuizApp
+//
+//  Created by Bartosz Mrugała on 14/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct PokemoonShadowQuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
